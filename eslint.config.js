79df50417dae3ee661b736a9c7e11ac429d083b3
yinm <yinm.cminor9@gmail.com>
2024-10-27 22:24:57 +0900

    Add React rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import yinmBase from "eslint-config-yinm";
+import yinmReact from "eslint-config-yinm/react";
 import yinmTypeScript from "eslint-config-yinm/typescript";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   ...yinmBase,
+  ...yinmReact,
   ...yinmTypeScript,
 
   {
     ignores: ["dist/*"],
   },
-]; // TODO: Add Preact ESLint rules
+];
