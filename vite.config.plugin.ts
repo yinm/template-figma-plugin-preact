293e5970dc2ee6cb965b1aa87a69fce300194155
yinm <yinm.cminor9@gmail.com>
2024-11-11 10:00:47 +0900

    Down the transpile version to the same version of Figma Plugin template
    If not, some ES features(e.g. Optional Chaining) occur Syntax Error

--- vite.config.plugin.ts
+++ vite.config.plugin.ts
@@ -1,11 +1,16 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 
+const target = "es2015";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src/plugin",
+  esbuild: {
+    target,
+  },
   build: {
-    target: "esnext",
+    target,
     outDir: path.resolve("dist"),
     emptyOutDir: false, // Suppress the warning
     rollupOptions: {
